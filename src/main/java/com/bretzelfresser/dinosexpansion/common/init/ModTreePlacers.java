3607fab95c5ed2829e01c7cce8a737bf309c4b1d
Bretzelfresser <bretzelfresser@gmail.com>
2026-07-01 10:16:37 +0200

    forgot to add the registry to git....

---
+++ src/main/java/com/bretzelfresser/dinosexpansion/common/init/ModTreePlacers.java
@@ -0,0 +1,30 @@
+package com.bretzelfresser.dinosexpansion.common.init;
+
+import com.bretzelfresser.dinosexpansion.DinosExpansion;
+import com.bretzelfresser.dinosexpansion.common.worldgen.PrehistoricFoliagePlacer;
+import com.bretzelfresser.dinosexpansion.common.worldgen.PrehistoricTrunkPlacer;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.world.level.levelgen.feature.foliageplacers.FoliagePlacerType;
+import net.minecraft.world.level.levelgen.feature.trunkplacers.TrunkPlacerType;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ModTreePlacers {
+    public static final DeferredRegister<TrunkPlacerType<?>> TRUNK_PLACER_TYPES =
+            DeferredRegister.create(Registries.TRUNK_PLACER_TYPE, DinosExpansion.MODID);
+
+    public static final DeferredRegister<FoliagePlacerType<?>> FOLIAGE_PLACER_TYPES =
+            DeferredRegister.create(Registries.FOLIAGE_PLACER_TYPE, DinosExpansion.MODID);
+
+    public static final DeferredHolder<TrunkPlacerType<?>, TrunkPlacerType<PrehistoricTrunkPlacer>> PREHISTORIC_TRUNK_PLACER =
+            TRUNK_PLACER_TYPES.register("prehistoric_trunk_placer", () -> new TrunkPlacerType<>(PrehistoricTrunkPlacer.CODEC));
+
+    public static final DeferredHolder<FoliagePlacerType<?>, FoliagePlacerType<PrehistoricFoliagePlacer>> PREHISTORIC_FOLIAGE_PLACER =
+            FOLIAGE_PLACER_TYPES.register("prehistoric_foliage_placer", () -> new FoliagePlacerType<>(PrehistoricFoliagePlacer.CODEC));
+
+    public static void register(IEventBus eventBus) {
+        TRUNK_PLACER_TYPES.register(eventBus);
+        FOLIAGE_PLACER_TYPES.register(eventBus);
+    }
+}
