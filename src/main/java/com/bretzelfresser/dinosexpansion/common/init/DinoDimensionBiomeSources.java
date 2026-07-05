74ffb4710f64040b2f31ca81e3fe0196819986fd
Bretzelfresser <bretzelfresser@gmail.com>
2026-07-05 13:14:14 +0200

    started with biome sources to stick the biomes to the terrain

---
+++ src/main/java/com/bretzelfresser/dinosexpansion/common/init/DinoDimensionBiomeSources.java
@@ -0,0 +1,12 @@
+package com.bretzelfresser.dinosexpansion.common.init;
+
+import net.minecraft.world.level.biome.BiomeSource;
+import net.minecraft.world.level.biome.MultiNoiseBiomeSource;
+
+public class DinoDimensionBiomeSources {
+
+
+    public static BiomeSource createDinoDimensionBiomeSource(){
+        MultiNoiseBiomeSource source = new
+    }
+}
