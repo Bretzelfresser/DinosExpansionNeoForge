9adad40abe9eeea33de5467251d2385a1e1c9257
Bretzelfresser <bretzelfresser@gmail.com>
2026-07-26 20:23:34 +0200

    changed the topror percentage value

--- src/main/java/com/bretzelfresser/dinosexpansion/config/DinosaurConfig.java
+++ src/main/java/com/bretzelfresser/dinosexpansion/config/DinosaurConfig.java
@@ -12,7 +12,7 @@ public class DinosaurConfig {
 
         builder.comment("When hitting the entity, the torpor gets buffered and then slowly added to the actual torpor of the entity.");
         builder.comment("This is the percentage coefficient of the stacked torpor added to the entity every tick");
-        PERCENTAGE_BUFFERED_TORPOR_REDUCTION = builder.defineInRange("percentage_buffered_torpor_reduction", 0.001, 0.0, 1.0);
+        PERCENTAGE_BUFFERED_TORPOR_REDUCTION = builder.defineInRange("percentage_buffered_torpor_reduction", 0.01, 0.0, 1.0);
 
         builder.comment("This is the flat rate of stacked torpor added to the entity every tick");
         FLAT_BUFFERED_TORPOR_REDUCTION = builder.defineInRange("flat_buffered_torpor_reduction", 0.01, 0.0, Double.MAX_VALUE);
